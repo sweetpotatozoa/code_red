95755c90bf72e10ce43cf47f3a1244b88902c1a8
sweetpotatozoa <tkddn3200@korea.ac.kr>
2024-06-07 19:06:11 +0900

    초기 세팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+
 function App() {
   return (
-    <div className='App'>
-      <h>Hello, Problem Solver!!!</h>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
