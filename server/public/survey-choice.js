d450290beedb48cbf366977f28806f66abf5ac5b
staend1 <ysyysy8656@gmail.com>
2024-06-10 17:19:35 +0900

    fix css 바꿈

--- server/public/survey-choice.js
+++ server/public/survey-choice.js
@@ -27,21 +27,21 @@
 
     const surveyContainer = document.createElement('div')
     surveyContainer.innerHTML = `
-          <div id="survey-popup">
-            <form id="surveyForm">
-              <label for="question">우리 제품을 통해 무엇을 이루고 싶나요?</label>
-              <div>
-                <input type="radio" name="answer" value="이탈률을 낮추고 싶어요" id="answer1">
-                <label for="answer1">이탈률을 낮추고 싶어요</label><br>
-                <input type="radio" name="answer" value="구매 전환율을 높이고 싶어요" id="answer2">
-                <label for="answer2">구매 전환율을 높이고 싶어요</label><br>
-                <input type="radio" name="answer" value="인터뷰이 모집을 쉽게 하고 싶어요" id="answer3">
-                <label for="answer3">인터뷰이 모집을 쉽게 하고 싶어요</label>
-              </div>
-              <button type="submit">제출</button>
-            </form>
-          </div>
-        `
+        <div id="survey-popup">
+          <form id="surveyForm">
+            <label for="question">우리 제품을 통해 무엇을 이루고 싶나요?</label>
+            <div>
+              <input type="radio" name="answer" value="이탈률을 낮추고 싶어요" id="answer1">
+              <label for="answer1">이탈률을 낮추고 싶어요</label><br>
+              <input type="radio" name="answer" value="구매 전환율을 높이고 싶어요" id="answer2">
+              <label for="answer2">구매 전환율을 높이고 싶어요</label><br>
+              <input type="radio" name="answer" value="인터뷰이 모집을 쉽게 하고 싶어요" id="answer3">
+              <label for="answer3">인터뷰이 모집을 쉽게 하고 싶어요</label>
+            </div>
+            <button type="submit">제출</button>
+          </form>
+        </div>
+      `
     console.log('Survey container created', surveyContainer)
 
     document.body.appendChild(surveyContainer)
