85936923f34125a9913104cf0f8632da50b8518a
staend1 <ysyysy8656@gmail.com>
2024-06-14 02:48:15 +0900

    fix 데이터정합성 수정

--- server/src/models/Survey.js
+++ server/src/models/Survey.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const surveySchema = new mongoose.Schema({
   customerId: { type: String, required: true },
   question: { type: String, required: true },
-  response: { type: String, default: '' },
   rating: { type: Number }, // 별점 질문을 위한 필드
   options: { type: [String] }, // 객관식 질문을 위한 필드
-  type: { type: String, required: true }, //질문의 종류를 나타내는 필드
+  type: { type: String, required: true }, // 질문의 종류를 나타내는 필드
+  multiChoiceResponse: { type: [String] }, // 다중 선택 객관식 질문을 위한 필드
 })
 
 module.exports = mongoose.model('Survey', surveySchema)
