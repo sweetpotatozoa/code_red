ec32effe299904e41a1a29a60bca797b13e35128
staend1 <ysyysy8656@gmail.com>
2024-06-13 16:04:24 +0900

    fix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const app = express()
 const compression = require('compression')
 const nunjucks = require('nunjucks')
 const cors = require('cors')
@@ -11,15 +10,12 @@ const indexRouter = require('./src/routes/index')
 const exampleRouter = require('./src/routes/Example_Route')
 const appliedSurveyRouter = require('./src/routes/Applied_Survey_Route')
 
+const app = express()
+
+// CORS 설정
 app.use(cors())
-app.set('view engine', 'ejs')
-app.engine('html', require('ejs').renderFile)
 
-nunjucks.configure('./src/views', {
-  express: app,
-  watch: true,
-})
-
+// 기타 미들웨어 설정
 app.use(compression())
 app.use(express.json({ limit: '200mb' }))
 app.use(
@@ -32,7 +28,16 @@ app.use(
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-// survey.js와 survey.css를 제공하는 경로 추가
+// 템플릿 엔진 설정
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+
+nunjucks.configure('./src/views', {
+  express: app,
+  watch: true,
+})
+
+// 정적 파일 경로 설정
 app.get('/survey.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'survey.js'))
 })
@@ -41,16 +46,18 @@ app.get('/survey.css', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'survey.css'))
 })
 
+// 라우터 설정
 app.use('/', indexRouter)
 app.use('/example', exampleRouter)
 app.use('/api/appliedSurvey', appliedSurveyRouter)
 
-// 에러 핸들링 미들웨어 추가
+// 에러 핸들링 미들웨어
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send('Something broke!')
 })
 
+// 데이터베이스 연결 후 서버 시작
 mongodb.runAfterAllConnected(() => {
   app.listen(configs.port, () => {
     console.log(`Server is running on port ${configs.port}`)
