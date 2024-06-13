81515f8da80bf7ead4f9c076e677886a9de5ae88
staend1 <ysyysy8656@gmail.com>
2024-06-13 13:21:58 +0900

    fix 웰컴카드 응답 저장 멘트 바뀜

--- server/src/models/Survey.js
+++ server/src/models/Survey.js
@@ -6,7 +6,7 @@ const surveySchema = new mongoose.Schema({
   response: { type: String, default: '' },
   rating: { type: Number }, // 별점 질문을 위한 필드
   options: { type: [String] }, // 객관식 질문을 위한 필드
-  type: { type: String, required: true },
+  type: { type: String, required: true }, //질문의 종류를 나타내는 필드
 })
 
 module.exports = mongoose.model('Survey', surveySchema)
