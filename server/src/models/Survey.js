b29b2bcf46957c58c4e8668a00bb7013c260c916
staend1 <ysyysy8656@gmail.com>
2024-06-19 19:05:21 +0900

    fix survey스키마 변경

--- server/src/models/Survey.js
+++ server/src/models/Survey.js
@@ -1,43 +1,45 @@
 const mongoose = require('mongoose')
 
+// Option Schema
+const OptionSchema = new mongoose.Schema({
+  id: { type: String, required: true }, // 공백 불가
+  value: String, // 공백 불가, rating의 경우 value가 없음
+  nextStepId: { type: String, required: true }, // 공백 가능
+})
+
+// Step Schema
+const StepSchema = new mongoose.Schema({
+  id: { type: String, required: true }, // 공백 불가
+  title: { type: String, required: true }, // 공백 불가
+  description: { type: String, required: true }, // 공백 가능
+  type: { type: String, required: true }, // 공백 불가
+  options: [OptionSchema],
+})
+
+// Trigger Schema
+const TriggerSchema = new mongoose.Schema({
+  id: { type: String, required: true }, // 공백 불가
+  title: { type: String, required: true }, // 공백 불가
+  description: { type: String, required: true }, // 공백 가능
+  type: { type: String, required: true }, // 공백 불가, 'newSession, scroll, exitIntent, click, url'
+  clickType: String, // 공백 불가, 'cssSelector or innerText'
+  clickValue: String, // 공백 불가, ' . # or text'
+  pageType: String, // 공백 불가, trigger.type이 url일 경우 없음
+  pageValue: String, // 공백 불가, trigger.type이 url일 경우 없음
+})
+
+// Main Survey Schema
 const surveySchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // customerId -> userId 변경
-  question: { type: String, required: true },
-  response: { type: String, default: '' },
-  rating: { type: Number }, // 별점 질문을 위한 필드
-  options: { type: [String] }, // 객관식 질문을 위한 필드
-  type: { type: String, required: true }, // 질문의 종류를 나타내는 필드
-  isDeploy: { type: Boolean, default: false }, // isActive -> isDeploy 변경
-  steps: [
-    {
-      id: { type: String, required: true }, // step에 id 추가
-      title: String,
-      description: String,
-      type: String,
-      options: [
-        {
-          id: { type: String, required: true }, // option에 id 추가
-          value: String,
-          nextStepId: String,
-        },
-      ],
-    },
-  ],
-  triggers: [
-    {
-      id: { type: String, required: true }, // trigger에 id 추가
-      title: String,
-      description: String,
-      type: String, // 기존 타입 값 (newSession, scroll, exitIntent, click, url)
-      clickType: String, // click일 경우의 타입 (cssSelector 또는 innerText)
-      clickValue: String, // selector 또는 text의 대체 필드
-    },
-  ],
+  userId: { type: String, required: true }, // 공백 불가
+  question: { type: String, required: true }, // 공백 불가
+  rating: { type: Number }, // 공백 불가
+  isDeploy: { type: Boolean, default: false }, // 공백 불가
+  steps: [StepSchema],
+  triggers: [TriggerSchema],
   delay: {
-    delayType: String, // 기존 displayOption
-    delayValue: Number, // 기존 cooldown
+    delayType: { type: String, required: true }, // 공백 불가
+    delayValue: { type: String, required: true }, // 공백 불가
   },
-  url: String, // buttonUrl -> url 변경
 })
 
 module.exports = mongoose.model('Survey', surveySchema)
