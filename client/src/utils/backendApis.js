f427ca4d2317fbf1d6818f20cd5556e8f59a652e
sweetpotatozoa <tkddn3200@korea.ac.kr>
2024-06-24 16:30:52 +0900

    Feat : backendapi목록 만들기

--- client/src/utils/backendApis.js
+++ client/src/utils/backendApis.js
@@ -27,8 +27,113 @@ class BackendApis {
   }
 
   async login(method = 'POST', params = {}) {
-    const result = await fetcher('/login', '', method, params)
+    const result = await fetcher('/api/auth/login', '', method, params)
     if (result?.status === 200) this.token = result.token
+    return result
+  }
+
+  async register(method = 'POST', params = {}) {
+    const result = await fetcher('/api/auth/register', '', method, params)
+    return result
+  }
+
+  async getSurveys(method = 'GET', params = {}) {
+    const result = await fetcher('/api/surveys', this.token, method, params)
+    return result
+  }
+
+  async isDeployToggle(method = 'PUT', params = {}) {
+    const result = await fetcher(
+      '/api/isDeployToggle',
+      this.token,
+      method,
+      params,
+    )
+    return result
+  }
+
+  async deleteSurvey(method = 'DELETE', params = {}) {
+    const result = await fetcher(
+      '/api/deleteSurvey',
+      this.token,
+      method,
+      params,
+    )
+    return result
+  }
+
+  async editSurveyPosition(method = 'PUT', params = {}) {
+    const result = await fetcher(
+      '/api/editSurveyPosition',
+      this.token,
+      method,
+      params,
+    )
+    return result
+  }
+
+  async getConnectStatus(method = 'GET', params = {}) {
+    const result = await fetcher(
+      '/api/connectStatus',
+      this.token,
+      method,
+      params,
+    )
+    return result
+  }
+
+  async getTemplates(method = 'GET', params = {}) {
+    const result = await fetcher('/api/templates', this.token, method, params)
+    return result
+  }
+
+  async createSurvey(method = 'POST', params = {}) {
+    const result = await fetcher('/api/templates', this.token, method, params)
+    return result
+  }
+
+  async editSurvey(method = 'GET', params = {}) {
+    const result = await fetcher('/api/edit', this.token, method, params)
+    return result
+  }
+
+  async updateSurvey(method = 'PUT', params = {}) {
+    const result = await fetcher(
+      '/api/surveyUpdate',
+      this.token,
+      method,
+      params,
+    )
+    return result
+  }
+
+  async deploySurvey(method = 'PUT', params = {}) {
+    const result = await fetcher(
+      '/api/surveyDeploy',
+      this.token,
+      method,
+      params,
+    )
+    return result
+  }
+
+  async getSummary(method = 'GET', params = {}) {
+    const result = await fetcher('/api/summary', this.token, method, params)
+    return result
+  }
+
+  async getResponse(method = 'GET', params = {}) {
+    const result = await fetcher('/api/response', this.token, method, params)
+    return result
+  }
+
+  async deleteResponse(method = 'DELETE', params = {}) {
+    const result = await fetcher('/api/response', this.token, method, params)
+    return result
+  }
+
+  async downloadResponses(method = 'GET', params = {}) {
+    const result = await fetcher('/api/download', this.token, method, params)
     return result
   }
 }
