afc3fef60488dfecec75b5b10c169a1b36f90e61
staend1 <ysyysy8656@gmail.com>
2024-11-16 21:05:09 +0900

    fix

--- client/src/pages/Salesmap/salesmap.js
+++ client/src/pages/Salesmap/salesmap.js
@@ -1,58 +1,31 @@
-import { useNavigate } from 'react-router-dom'
-import styles from './salesmap.module.css' // 일단 Home 스타일 재사용
-import { useState } from 'react'
+import { useEffect } from 'react'
+import styles from './Salesmap.module.css'
 
 const Salesmap = () => {
-  const [userInfo, setUserInfo] = useState('')
-  const navigate = useNavigate()
+  useEffect(() => {
+    // 스크립트를 동적으로 추가
+    const script = document.createElement('script')
+    script.src = 'https://salesmap.kr/web-form-loader-v3.js'
+    script.id = 'loadFormScript'
+    script.async = true
+    script.onload = () => {
+      window.SmFormSettings && window.SmFormSettings.loadForm()
+    }
+    document.body.appendChild(script)
 
-  const goToHome = () => {
-    navigate('/')
-  }
+    return () => {
+      // 클린업
+      const scriptElement = document.getElementById('loadFormScript')
+      if (scriptElement) scriptElement.remove()
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
-      <div className={styles.sideBar}>
-        <img
-          src='/images/logo.svg'
-          className={styles.logo}
-          onClick={goToHome}
-        ></img>
-        <div className={styles.navBar}>
-          <div className={styles.nav} onClick={goToHome}>
-            설문조사
-          </div>
-        </div>
-        <div className={styles.user}>
-          <div className={styles.welcome}>
-            안녕하세요,
-            <br />
-            {userInfo.realName}님!
-          </div>
-        </div>
-      </div>
-      <div className={styles.main}>
-        <div
-          id='salesmap-web-form'
-          data-web-form='https://salesmap.kr/web-form/ebf2f1b5-b435-4ba0-9821-4b8c2ef31cb8'
-          dangerouslySetInnerHTML={{
-            __html: `
-              <script>
-                !(function (window, document) {
-                  var currentScript = document.currentScript;
-                  var scriptElement = document.createElement('script');
-                  scriptElement.onload = function () {
-                    window.SmFormSettings.loadForm();
-                  };
-                  scriptElement.id = 'loadFormScript';
-                  scriptElement.src = 'https://salesmap.kr/web-form-loader-v3.js';
-                  currentScript.parentNode.insertBefore(scriptElement, currentScript);
-                })(window, document);
-              </script>
-            `,
-          }}
-        />
-      </div>
+      <div
+        id='salesmap-web-form'
+        data-web-form='https://salesmap.kr/web-form/ebf2f1b5-b435-4ba0-9821-4b8c2ef31cb8'
+      />
     </div>
   )
 }
