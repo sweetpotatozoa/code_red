7ff38e2fc2c60ae62dcd3e89d6a5c7df2b4565a5
staend1 <ysyysy8656@gmail.com>
2024-12-12 15:12:38 +0900

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -14,92 +14,92 @@ import Edit from './pages/Edit/Edit'
 import Salesmap from './pages/Salesmap/salesmap'
 
 function App() {
-  const location = useLocation()
+ const location = useLocation()
 
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.src = `https://www.googletagmanager.com/gtag/js?id=G-WGX1YS6SP3`
-    script.async = true
-    document.head.appendChild(script)
+ useEffect(() => {
+   const script = document.createElement('script')
+   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-WGX1YS6SP3'
+   script.async = true
+   document.head.appendChild(script)
 
-    window.dataLayer = window.dataLayer || []
-    function gtag() {
-      window.dataLayer.push(arguments)
-    }
-    gtag('js', new Date())
-    gtag('config', 'G-WGX1YS6SP3')
-  }, [])
+   window.dataLayer = window.dataLayer || []
+   function gtag() {
+     window.dataLayer.push(arguments)
+   }
+   gtag('js', new Date())
+   gtag('config', 'G-WGX1YS6SP3')
+ }, [])
 
-  useEffect(() => {
-    if (window.gtag) {
-      window.gtag('event', 'page_view', {
-        page_path: location.pathname,
-      })
-    }
-  }, [location])
+ useEffect(() => {
+   if (window.gtag) {
+     window.gtag('event', 'page_view', {
+       page_path: location.pathname,
+     })
+   }
+ }, [location])
 
-  return (
-    <Routes>
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route
-        path='/'
-        element={
-          <PrivateRoute>
-            <Home />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path='/onboarding'
-        element={
-          <PrivateRoute>
-            <Onboarding />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path='/responses/:id'
-        element={
-          <PrivateRoute>
-            <Responses />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path='/summary/:id'
-        element={
-          <PrivateRoute>
-            <Summary />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path='/templates'
-        element={
-          <PrivateRoute>
-            <Templates />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path='/edit/:id'
-        element={
-          <PrivateRoute>
-            <Edit />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path='/salesmap'
-        element={
-          <PrivateRoute>
-            <Salesmap />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
-  )
+ return (
+   <Routes>
+     <Route path='/login' element={<Login />} />
+     <Route path='/register' element={<Register />} />
+     <Route
+       path='/'
+       element={
+         <PrivateRoute>
+           <Home />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path='/onboarding'
+       element={
+         <PrivateRoute>
+           <Onboarding />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path='/responses/:id'
+       element={
+         <PrivateRoute>
+           <Responses />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path='/summary/:id'
+       element={
+         <PrivateRoute>
+           <Summary />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path='/templates'
+       element={
+         <PrivateRoute>
+           <Templates />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path='/edit/:id'
+       element={
+         <PrivateRoute>
+           <Edit />
+         </PrivateRoute>
+       }
+     />
+     <Route
+       path='/salesmap'
+       element={
+         <PrivateRoute>
+           <Salesmap />
+         </PrivateRoute>
+       }
+     />
+   </Routes>
+ )
 }
 
 export default App
